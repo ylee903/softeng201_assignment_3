eeedfe6dfb040bf06e535da7ab75da85dd54af73
ylee903 <79842277+ylee903@users.noreply.github.com>
2024-05-29 03:30:36 +1200

    attempted to make a initial start on the stuff thingy majiggy

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,6 +1,8 @@
 package nz.ac.auckland.se281;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /** This class is the main entry point. */
 public class MapEngine {
@@ -15,6 +17,30 @@ public class MapEngine {
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
     // add code here to create your data structures
+
+    // Create a map to store countries
+    Map<String, Country> countryMap = new HashMap<>();
+
+    // Parse countries
+    for (String country : countries) {
+      String[] parts = country.split(",");
+      Country c = new Country();
+      c.setName(parts[0]);
+      c.setContinent(parts[1]);
+      c.setTaxFees(Integer.parseInt(parts[2]));
+      countryMap.put(c.getName(), c);
+    }
+
+    // Parse adjacencies
+    for (String adjacency : adjacencies) {
+      String[] parts = adjacency.split(",");
+      Country country1 = countryMap.get(parts[0]);
+      Country country2 = countryMap.get(parts[1]);
+      if (country1 != null && country2 != null) {
+        country1.addAdjacentCountry(country2);
+        country2.addAdjacentCountry(country1);
+      }
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
